68b7cbc73b9fc8f28f6eababd5643f83413ec4e0
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-03-23 22:12:40 -0500

    Create Product_Categories_Minor.sql

---
+++ sql/tables/Product_Categories_Minor.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Product_Categories_Minor` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `product_categories_major_id` INT(10) UNSIGNED NOT NULL,
+  `name` CHAR(50) COLLATE UTF8_UNICODE_CI NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `product_categories_major_id` (`product_categories_major_id`),
+  CONSTRAINT `Product_Categories_Minor_ibfk_1` FOREIGN KEY (`product_categories_major_id`) REFERENCES `Product_Categories_Minor` (`id`) ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE=UTF8_UNICODE_CI;
