da06c8fc17f17cdbde2072187408774878a020f9
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-04-21 18:54:26 -0500

    Create View_Product_Availability.sql

---
+++ sql/views/View_Product_Availability.sql
@@ -0,0 +1,8 @@
+CREATE VIEW `View_Product_Availability`
+AS
+  SELECT `a`.`id`         AS `id`,
+         `a`.`starts_on`  AS `starts_on`,
+         `a`.`ends_on`    AS `ends_on`,
+         `a`.`note`       AS `note`,
+         `a`.`created_on` AS `created_on`
+  FROM   `Product_Availability` `a`; 
