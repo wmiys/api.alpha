9b0554f97b92e899c9b4524626d1d23cae198f70
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-04-21 18:58:46 -0500

    add product_id to the view

--- sql/views/View_Product_Availability.sql
+++ sql/views/View_Product_Availability.sql
@@ -1,6 +1,7 @@
 CREATE VIEW `View_Product_Availability`
 AS
   SELECT `a`.`id`         AS `id`,
+         `a`.`product_id`  AS `product_id`,
          `a`.`starts_on`  AS `starts_on`,
          `a`.`ends_on`    AS `ends_on`,
          `a`.`note`       AS `note`,
