d300828ce37555fafbe60fdfd7b605bb534046a1
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-04-28 20:40:05 -0500

    use location ids as input parms

--- sql/functions/Miles_Between.sql
+++ sql/functions/Miles_Between.sql
@@ -1,10 +1,18 @@
 DELIMITER $$
-CREATE FUNCTION `Miles_Between`(
-	lat1 DECIMAL(11,7), 
-    lng1 DECIMAL(11,7), 
-    lat2 DECIMAL(11,7), 
-    lng2 DECIMAL(11,7)
+CREATE DEFINER=`rrickgau_main`@`24.13.68.79` FUNCTION `Miles_Between`(
+	location_id_1 INT UNSIGNED,
+    location_id_2 INT UNSIGNED
 ) RETURNS double
     DETERMINISTIC
-RETURN ACOS( SIN(lat1*PI()/180)*SIN(lat2*PI()/180) + COS(lat1*PI()/180)*COS(lat2*PI()/180)*COS(lng2*PI()/180-lng1*PI()/180) ) * 3959$$
+BEGIN
+	DECLARE lat1 DECIMAL(11,7);
+	DECLARE lng1 DECIMAL(11,7);
+	DECLARE lat2 DECIMAL(11,7);
+	DECLARE lng2 DECIMAL(11,7);
+    
+    SELECT l.lat, l.lng INTO lat1, lng1 FROM Locations l WHERE l.id = location_id_1;	-- get the first location lat/lng points
+    SELECT l.lat, l.lng INTO lat2, lng2 FROM Locations l WHERE l.id = location_id_2;	-- get the second location lat/lng points
+    
+RETURN ACOS( SIN(lat1*PI()/180)*SIN(lat2*PI()/180) + COS(lat1*PI()/180)*COS(lat2*PI()/180)*COS(lng2*PI()/180-lng1*PI()/180) ) * 3959;
+END$$
 DELIMITER ;
