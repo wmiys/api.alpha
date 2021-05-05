6f32f4a1dfdd1c39c03b1b770a605f619c13f6bd
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-05-04 21:25:02 -0500

    add separate flag

--- src/api_wmiys/product_categories/Product_Categories.py
+++ src/api_wmiys/product_categories/Product_Categories.py
@@ -14,8 +14,36 @@ class ProductCategories:
     #------------------------------------------------------
     @staticmethod
     def getAll():
-        categories = DB.getProductCategories()
-        return categories
+        categoryTable = DB.getProductCategories()
+        return categoryTable
+
+    @staticmethod
+    def getAllSeperate():
+        categoryTable = DB.getProductCategories()
+
+        majors = []
+        minors = []
+        subs = []
+
+        Major = namedtuple('Major', ['id', 'name'])
+        Minor = namedtuple('Minor', ['id', 'name', 'parent_id'])
+        Sub   = namedtuple('Sub', ['id', 'name', 'parent_id'])
+
+        for row in categoryTable:
+            major = dict(id=row.major_id, name=row.major_name)
+            minor = dict(id=row.minor_id, name=row.minor_name, parent_id=row.major_id)
+            sub = dict(id=row.sub_id, name=row.sub_name, parent_id=row.minor_id)
+            
+            if major not in majors:
+                majors.append(major)
+            if minor not in minors:
+                minors.append(minor)
+            if sub not in subs:
+                subs.append(sub)
+
+
+        return dict(major=majors, minor=minors, sub=subs)
+        
 
     #------------------------------------------------------
     # Retrieve all major categories
