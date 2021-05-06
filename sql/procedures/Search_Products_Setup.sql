2057f5c15c63a0d72a3bf7a3c8446a68eec4eb41
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-05-05 23:11:43 -0500

    Create Search_Products_Setup.sql

---
+++ sql/procedures/Search_Products_Setup.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+CREATE PROCEDURE `Search_Products_Setup`(
+    IN in_dropoff_location_id INT UNSIGNED, 
+    IN in_starts_on DATE,
+    IN in_ends_on DATE
+)
+BEGIN
+    
+    CREATE TEMPORARY TABLE IF NOT EXISTS tmp_products_available (
+		product_id INT UNSIGNED
+	);
+    
+    DELETE FROM tmp_products_available;
+    
+    INSERT INTO tmp_products_available (product_id)
+    SELECT p.id
+	FROM Products p
+    WHERE 
+        IS_PRODUCT_AVAILABLE(p.id, in_starts_on, in_ends_on) = TRUE                         -- date ranges don't conflict with any existing product availability records
+        AND  MILES_BETWEEN(p.location_id, in_dropoff_location_id) <= p.dropoff_distance;    -- product dropoff distance must be within distance between the renter and the lender
+END$$
+DELIMITER ;
