1b30051252994789a1840c9cadca32e3728cbc7a
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-09-26 02:10:41 -0500

    move product images into module

--- src/api_wmiys/models/product_image.py
+++ src/api_wmiys/models/product_image.py
@@ -1,72 +1,98 @@
-from ..db import DB
-from ..common import utilities
-
-
+from __future__ import annotations
 import os
+from ..db import DB
+from ..common import utilities, user_image
 
 class ProductImage:
-
-    
     # LOCAL_SERVER_IMAGE_DIRECTORY = 'product-images/images'
 
     LOCAL_SERVER_IMAGE_DIRECTORY = "http://10.0.0.82/files/api.wmiys/src/product-images/images"
     LOCAL_SERVER_IMAGE_DIRECTORY_RELATIVE = 'product-images/images'
 
-
+    #----------------------------------------------------------
+    # Constructor
+    #----------------------------------------------------------
     def __init__(self, newID=None, product_id=None, file_name=None, created_on=None):
         self.id = newID
         self.product_id = product_id
         self.file_name = file_name
         self.created_on = created_on
 
-
-    def insert(self):
-        """Insert the product image into the database
-
-        Returns:
-            bool: if it was successful or not
-        """
+    #----------------------------------------------------------
+    # Insert the product image into the database
+    # 
+    # Returns:
+    #   bool: if it was successful or not
+    #----------------------------------------------------------
+    def insert(self) -> bool:
         if None in [self.product_id, self.file_name]:
             return False
         
-        self.id = DB.insertProductImage(product_id=self.product_id, file_name=self.file_name)
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(False)
+
+        sql = """
+        INSERT INTO Product_Images 
+        (product_id, file_name) VALUES
+        (%s, %s)
+        """
+
+        parms = (self.product_id, self.file_name)
+
+        cursor.execute()
+        db.commit()
+        self.id = cursor.lastrowid
+
+        db.close()
+
         return True
 
-    
-    def load(self):
-        """Loads the object properties values from the database
 
-        Returns:
-            bool: whether or not the load was successful.
-        """
+    #----------------------------------------------------------
+    # Loads the object properties values from the database
+    # 
+    # Returns:
+    #   bool: if it was successful or not
+    #----------------------------------------------------------
+    def load(self) -> bool:
         if not self.id:
             return False
 
-        imageData = DB.getProductImage(self.id)
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(True)
+
+        sql = """
+        SELECT   *
+        FROM     Product_Images pi
+        WHERE    pi.id = %s
+        LIMIT 1
+        """
+
+        parms = (self.id,)
+        cursor.execute()
+        imageData = cursor.fetchone()
 
         if not imageData:
             return False    # no records were found with the given ID
 
+        self.product_id = imageData.get('product_id', None)
+        self.file_name  = imageData.get('file_name', None)
+        self.created_on = imageData.get('created_on', None)
 
-        self.product_id = imageData.product_id
-        self.file_name = imageData.file_name
-        self.created_on = imageData.created_on
+        db.close()
 
         return True
-
-
+    #----------------------------------------------------------
+    # Takes a raw image file, saves it locally, and sets the image 
+    # field in the database to the image file name as saved on the server.
+    # 
+    # Parms:
+    #     newImageFile (object): the raw image file
+    #     relative_image_directory_path (str): the folder name to save the image to    
+    #----------------------------------------------------------
     def setImagePropertyFromImageFile(self, newImageFile: object, relative_image_directory_path: str):
-        from ..common import user_image
-        """Takes a raw image file, saves it locally, and sets the image field in the database to the image file name as saved on the server.
-
-        ---
-        Args:
-        
-        - newImageFile (object): the raw image file
-        - relative_image_directory_path (str): the folder name to save the image to
-        """
-
-
         # remove the old image
         if self.file_name:
             os.remove(os.path.join(relative_image_directory_path, self.file_name))
@@ -75,47 +101,94 @@ class ProductImage:
         newImageFileName = utilities.getUUID(True) + productImage.getFileExtension()
         self.file_name = productImage.saveImageFile(relative_image_directory_path, newImageFileName)
 
-    def toDict(self):
-        """Returns the object as a dict
-        """
+
+    #----------------------------------------------------------
+    # Returns the object as a dict
+    #----------------------------------------------------------
+    def toDict(self) -> dict:
         return dict(id=self.id, product_id=self.product_id, file_name=self.file_name, created_on=self.created_on)
 
 
+    #----------------------------------------------------------
+    # Retrieve all of the product images for a product
+    #
+    # Parms:
+    #   product_id (int): product id
+    #
+    # Returns a list:
+    #   all of the product images that belong to a product
+    #----------------------------------------------------------
     @staticmethod
-    def getAll(product_id: int):
-        """Retrieve all of the product images for a product
-
-        Args:
-            product_id (int): product id
-
-        Returns:
-            list: all of the product images that belong to a product
-        """
-        images = DB.getProductImages(product_id)
-        
+    def getAll(product_id: int) -> list[dict]:
+        images = ProductImage._getAllProductImageRecords(product_id)
 
         # prepend the absolute url for each image file_name
-        result = []
-        for img in images:
-            imgDict = img._asdict()
-            imgDict['file_name'] = ProductImage.LOCAL_SERVER_IMAGE_DIRECTORY + '/' + imgDict['file_name']
-            result.append(imgDict)
+        for image in images:
+            image['file_name'] = ProductImage.LOCAL_SERVER_IMAGE_DIRECTORY + '/' + image['file_name']
 
-        return result
+        return images
 
+    #----------------------------------------------------------
+    # Delete all the product images for a product
+    #----------------------------------------------------------
     @staticmethod
     def deleteAll(product_id: int):
-        """Delete all the product images for a product
+        ProductImage._deleteAllImageFiles(product_id)
+        record_count = ProductImage._deleteAllImageDatabaseRecords(product_id)
+        return record_count
+    
+    
+    #----------------------------------------------------------
+    # Deletes all the image files from storage that belong to the 
+    # given product id.
+    #----------------------------------------------------------
+    @staticmethod
+    def _deleteAllImageFiles(product_id: int):
+        for img in ProductImage._getAllProductImageRecords(product_id):
+            os.remove(ProductImage.LOCAL_SERVER_IMAGE_DIRECTORY_RELATIVE + '/' + img.get('file_name'))
+
+    #----------------------------------------------------------
+    # Deletes all the image files from the database that belong 
+    # to the given product id.
+    #----------------------------------------------------------
+    @staticmethod
+    def _deleteAllImageDatabaseRecords(product_id: int) -> int:
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(True)
+
+        sql = 'DELETE FROM Product_Images WHERE product_id = %s'
+
+        parms = (product_id,)
+        cursor.execute()
+        db.commit()
+        record_count = cursor.rowcount
+        db.close()
+
+        return record_count
+
+    #----------------------------------------------------------
+    # Retrieve all the product image database records that belong
+    # to the given product.
+    #----------------------------------------------------------
+    @staticmethod
+    def _getAllProductImageRecords(product_id: int) -> list[dict]:
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(True)
+
+        sql = """
+        SELECT   *
+        FROM     Product_Images pi
+        WHERE    pi.product_id = %s
         """
-        images = DB.getProductImages(product_id)
 
-        for img in images:
-            os.remove(ProductImage.LOCAL_SERVER_IMAGE_DIRECTORY_RELATIVE + '/' + img.file_name)
+        parms = (product_id,)
+        cursor.execute()
+        images = cursor.fetchall()
+        db.close()
 
-        return DB.deleteProductImages(product_id)
-
-        
-
+        return images
         
 
 
