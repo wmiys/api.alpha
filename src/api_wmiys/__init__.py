c9c0b2cf72e5e551f351e3e0bfac07f432e77b28
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-09-26 02:18:43 -0500

    add routes to readme and top __init__

--- src/api_wmiys/__init__.py
+++ src/api_wmiys/__init__.py
@@ -1,3 +1,25 @@
+"""
+Available Routes:
+
+
+Resource             | URI
+---------------------|---------------------------------------------------------------------------
+User                 | /users/:user_id
+Products             | /users/:user_id/products/:product_id
+Product Availability | /users/:user_id/products/:product_id/availability/:product_availability_id
+Product Images       | /users/:user_id/products/:product_id/images/:product_image_id
+Listings             | /listings/:product_id
+Product Categories   | /product-categories
+Login                | /login
+Locations            | /locations/:location_id
+Search Locations     | /search/locations
+Search Products      | /search/products
+
+"""
+
+
+
+
 from flask import Flask
 from flask_cors import CORS
 from .common import CustomJSONEncoder
