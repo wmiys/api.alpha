27bac90faa03d48005f6ef343a22f23da4caf078
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-10-06 07:42:11 -0500

    re-create product requests database table

--- sql/tables/Product_Requests.sql
+++ sql/tables/Product_Requests.sql
@@ -1,18 +1,10 @@
 CREATE TABLE Product_Requests (
-    id INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    renter_id INT UNSIGNED NOT NULL,
-    product_id INT UNSIGNED NOT NULL,
-    starts_on DATE NOT NULL,
-    ends_on DATE NOT NULL,
-    price_full DECIMAL(10,2) UNSIGNED,
-    price_half DECIMAL(10,2) UNSIGNED,
-    dropoff_location_id INT UNSIGNED NOT NULL,
-    status ENUM ('pending', 'accepted', 'denied', 'expired') NOT NULL DEFAULT 'pending',
-    responded_on DATE,
-    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    payment_id CHAR(36) NOT NULL,
+    session_id CHAR(255) NOT NULL,
+    status ENUM('pending','accepted','denied','expired') NOT NULL DEFAULT 'pending',
+    responded_on DATETIME DEFAULT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (renter_id) REFERENCES Users (id) ON UPDATE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES Products (id) ON UPDATE CASCADE,
-    FOREIGN KEY (dropoff_location_id) REFERENCES Locations(id) ON UPDATE CASCADE
+    FOREIGN KEY (payment_id) REFERENCES Payments(id) ON UPDATE CASCADE
 );
-    
