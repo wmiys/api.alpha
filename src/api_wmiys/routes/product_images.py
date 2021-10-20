5b2ca1d9a351ba6552b4ec970daa5b4a41901d29
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-10-19 21:01:10 -0500

    don't check for authorization if it's a Get request (#167)

--- src/api_wmiys/routes/product_images.py
+++ src/api_wmiys/routes/product_images.py
@@ -18,18 +18,17 @@ bpProductImages = flask.Blueprint('bpProductImages', __name__)
 @bpProductImages.route('', methods=['GET', 'POST', 'DELETE'])
 @security.login_required
 def searchAll(product_id: int):
-    # make sure the user is authorized
-    if not product.doesUserOwnProduct(product_id, security.requestGlobals.client_id):
-        return ('', HTTPStatus.FORBIDDEN.value)
-
     if flask.request.method == 'GET':
         # all we need to do is fetch all the product images
         return flask.jsonify(ProductImage.getAll(product_id))
     
-    elif flask.request.method == 'DELETE':
+    # make sure the user is authorized
+    if not product.doesUserOwnProduct(product_id, security.requestGlobals.client_id):
+        return ('', HTTPStatus.FORBIDDEN.value)
+    
+    if flask.request.method == 'DELETE':
         ProductImage.deleteAll(product_id)
         return ('', HTTPStatus.NO_CONTENT.value)
-
     
     # if we get to this point, we are creating a new product image record
     imagesData: dict = flask.request.files.to_dict()
