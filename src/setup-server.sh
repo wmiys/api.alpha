957b882864bd1469893afa335828db8ad7b657a8
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-10-27 22:26:55 -0500

    Fix the start/test server scripts

--- src/setup-server.sh
+++ src/setup-server.sh
@@ -1,19 +1,16 @@
-mod_wsgi-express setup-server /var/www/wmiys/api/api_wmiys.wsgi \
+mod_wsgi-express setup-server \
 --user www-data  \
 --group www-data  \
 --server-name api.wmiys.com  \
---port 80   \
+--port 81   \
 --access-log  \
 --log-level info   \
 --compress-responses \
---processes 3 \
---startup-timeout 30   \
 --server-root /etc/api.wmiys.com \
 --host 104.225.208.116 \
 --document-root /var/www/wmiys/api/api_wmiys/static \
---https-port 443 \
---https-only \
---ssl-certificate-file /etc/letsencrypt/live/api.wmiys.com/fullchain.pem \
---ssl-certificate-key-file /etc/letsencrypt/live/api.wmiys.com/privkey.pem \
---setup-only
+--setup-only \
+/var/www/wmiys/api/api_wmiys.wsgi
 
+# restart the server
+/etc/api.wmiys.com/apachectl restart
