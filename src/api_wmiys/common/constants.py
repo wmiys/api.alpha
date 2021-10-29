37dbf82f12551edcc5b573fba1546ddcee984f08
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-10-28 19:20:49 -0500

    add dev constant for product image urls

---
+++ src/api_wmiys/common/constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+# Contains some constant values we need when doing development
+class DevConfig(Enum):
+    STATIC_URL_PREFIX = 'http://10.0.0.82:5000/'
--- src/run.py
+++ src/run.py
@@ -1,4 +1,6 @@
 from api_wmiys import app 
+from api_wmiys.common import constants, user_image
 
 if __name__ == "__main__":
+    user_image.STATIC_URL_PREFIX = constants.DevConfig.STATIC_URL_PREFIX.value
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
