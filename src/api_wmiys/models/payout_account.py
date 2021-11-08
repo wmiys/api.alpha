b15da3f1cfd440920d48367b2a3f4c696428786c
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-11-08 08:11:33 -0600

    Branch issue 193 (#194)
    * create initial route and sql table
    
    * insert the record into the database
    
    * create payout class
    
    * get all or a single record(s)
    
    * update a record
    
    * add payout account to user view
    
    * add user_id to the metadata for stripe account

---
+++ src/api_wmiys/models/payout_account.py
@@ -0,0 +1,141 @@
+
+from __future__ import annotations
+import uuid
+from datetime import datetime
+import stripe
+from wmiys_common import keys
+from ..db import DB, sqlBoolToPython, SqlBool
+
+
+stripe.api_key = keys.payments.test
+
+
+#------------------------------------------------------
+# Create a new stripe account
+#------------------------------------------------------
+def getNewStripeAccount(user_id: int) -> stripe.Account:
+    
+    return stripe.Account.create(
+        type='express',
+        metadata=dict(user_id=user_id)
+    )
+
+#------------------------------------------------------
+# Get all payout accounts owned by the given user
+#------------------------------------------------------
+def getAll(user_id: int) -> list[dict]:
+    db = DB()
+    db.connect()
+    cursor = db.getCursor(True)
+
+    sql = 'SELECT * FROM Payout_Accounts WHERE user_id=%s ORDER BY created_on DESC'
+    parms = (user_id,)
+    cursor.execute(sql, parms)
+    records: list = cursor.fetchall()
+
+    db.close()
+
+    # transform all sql bool types into python bools
+    for i, record in enumerate(records):
+        records[i]['confirmed'] = sqlBoolToPython(SqlBool(record.get('confirmed')))
+    
+    return records
+
+
+class PayoutAccount:
+
+    def __init__(self, id: uuid.UUID=None, user_id: int=None, account_id: str=None, created_on: datetime=None, confirmed: bool=False):
+        self.id         = id
+        self.user_id    = user_id
+        self.account_id = account_id
+        self.created_on = created_on or datetime.now()
+        self.confirmed  = confirmed
+
+    #------------------------------------------------------
+    # Insert the object into the database
+    #
+    # Returns a bool:
+    #   true: insert was successful
+    #   false: insert was not successful
+    #------------------------------------------------------
+    def insert(self) -> bool:
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(False)
+
+        sql = 'INSERT INTO Payout_Accounts (id, user_id, account_id, created_on) VALUES (%s, %s, %s, %s)'
+        parms = (str(self.id), self.user_id, self.account_id, self.created_on)
+
+        try:
+            cursor.execute(sql, parms)
+            db.commit()
+            success = True
+        except Exception as e:
+            success = False
+            print(e)
+        finally:
+            db.close()
+        
+        return success
+
+
+
+
+    def load(self):
+        db_result = self.get()
+
+        if not db_result:
+            return False
+
+        # self.id         = db_result.get('id')
+        self.user_id    = db_result.get('user_id')
+        self.account_id = db_result.get('account_id')
+        self.created_on = db_result.get('created_on')
+        self.confirmed  = db_result.get('confirmed')
+
+        return True
+
+
+    #------------------------------------------------------
+    # Get a single payout account
+    #------------------------------------------------------
+    def get(self) -> dict:
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(True)
+
+        sql = 'SELECT * FROM Payout_Accounts WHERE id=%s and user_id=%s LIMIT 1'
+        parms = (str(self.id), self.user_id)
+        cursor.execute(sql, parms)
+        result = cursor.fetchone()
+
+        db.close()
+
+        if result:
+            result['confirmed'] = sqlBoolToPython(SqlBool(result.get('confirmed')))
+
+        return result
+
+    #------------------------------------------------------
+    # Update the object
+    #------------------------------------------------------
+    def update(self) -> int:
+        db = DB()
+        db.connect()
+        cursor = db.getCursor(False)
+
+        sql = 'UPDATE Payout_Accounts SET confirmed = %s WHERE id=%s and user_id=%s'
+        parms = (self.confirmed, str(self.id), self.user_id)
+
+        try:
+            cursor.execute(sql, parms)
+            db.commit()
+            row_count = cursor.rowcount
+        except Exception as e:
+            print(e)
+            row_count = -1
+        finally:
+            db.close()
+
+        return row_count
+
---
+++ src/api_wmiys/routes/payout_accounts.py
@@ -0,0 +1,87 @@
+"""
+Package:        payout_accounts
+Url Prefix:     /payout-accounts/
+Description:    Routing for creating payout accounts
+
+This is not accessible to any 3rd party clients.
+Only accessible by the front-end.
+"""
+import uuid
+import flask
+from wmiys_common import utilities
+from ..common import security
+from ..models import PayoutAccount, payout_account
+from http import HTTPStatus
+
+bp_payout_accounts = flask.Blueprint('bp_payout_accounts', __name__)
+
+
+#------------------------------------------------------
+# Create a new payout account
+#------------------------------------------------------
+@bp_payout_accounts.post('')
+@security.no_external_requests
+@security.login_required
+def post():
+    new_stripe_account = payout_account.getNewStripeAccount(security.requestGlobals.client_id)
+
+    account = PayoutAccount(
+        id         = utilities.getUUID(False),
+        user_id    = security.requestGlobals.client_id,
+        account_id = new_stripe_account.stripe_id
+    )
+
+    if not account.insert():
+        return ('Did not insert!', HTTPStatus.BAD_REQUEST.value)
+    
+    return flask.jsonify(account.__dict__)
+
+
+#------------------------------------------------------
+# Get all payout accounts owned by the user
+#------------------------------------------------------
+@bp_payout_accounts.get('')
+@security.no_external_requests
+@security.login_required
+def getAll():
+    accounts = payout_account.getAll(security.requestGlobals.client_id)
+    return flask.jsonify(accounts)
+    
+
+#------------------------------------------------------
+# Get a single payout account 
+#------------------------------------------------------
+@bp_payout_accounts.get('<uuid:payout_account_id>')
+@security.no_external_requests
+@security.login_required
+def get(payout_account_id: uuid.UUID):
+    account = PayoutAccount(
+        id      = payout_account_id,
+        user_id = security.requestGlobals.client_id
+    )
+    
+    return flask.jsonify(account.get())
+
+
+#------------------------------------------------------
+# Update a record
+#------------------------------------------------------
+@bp_payout_accounts.put('<uuid:payout_account_id>')
+@security.no_external_requests
+@security.login_required
+def put(payout_account_id: uuid.UUID):
+    account = PayoutAccount(
+        id      = payout_account_id,
+        user_id = security.requestGlobals.client_id
+    )
+
+    print(flask.json.dumps(flask.request.form.to_dict(), indent=4))
+
+    if flask.request.form.get('confirmed', False) in [True, "true", "True"]:
+        account.confirmed = True
+    else:
+        account.confirmed = False
+
+    account.update()
+    
+    return flask.jsonify(account.get())
