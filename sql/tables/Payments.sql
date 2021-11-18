1987c584ea74cda17a7ed54293ad5819b63514b0
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-11-18 07:53:13 -0600

    remove session_id from payments model

--- sql/tables/Payments.sql
+++ sql/tables/Payments.sql
@@ -9,7 +9,6 @@ CREATE TABLE Payments
     price_full DECIMAL(10,2) UNSIGNED NOT NULL,
     fee_renter DECIMAL(3,2) NOT NULL DEFAULT '0.00',
     fee_lender DECIMAL(3,2) unsigned NOT NULL DEFAULT '0.00',
-    payment_session_id VARCHAR(255),
     created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES Products(id) ON UPDATE CASCADE,
