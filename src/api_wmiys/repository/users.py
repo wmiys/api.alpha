84f56ba2a918b7804a6f01be5980b84b5d3c442f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-03-23 12:40:20 -0500

    insert a new user

--- src/api_wmiys/repository/users.py
+++ src/api_wmiys/repository/users.py
@@ -4,8 +4,11 @@ Users repository
 
 """
 from __future__ import annotations
+
 import pymysql.commands as sql_engine
 from pymysql.structs import DbOperationResult
+from pymysql.connection import ConnectionPrepared
+
 from api_wmiys.domain import models
 
 
@@ -13,7 +16,7 @@ from api_wmiys.domain import models
 SQL_SELECT = 'SELECT * FROM View_Users WHERE id=%s LIMIT 1;'
 
 
-SQL_UPDATE = """
+SQL_UPDATE = '''
     UPDATE Users 
     SET
         email      = %s,
@@ -23,7 +26,16 @@ SQL_UPDATE = """
         birth_date = %s
     WHERE
         id =         %s;
-"""
+'''
+
+
+
+SQL_INSERT = '''
+    INSERT INTO 
+        Users (email, password, name_first, name_last, birth_date)
+    VALUES
+        (%s, %s, %s, %s, %s);
+'''
 
 
 #------------------------------------------------------
@@ -34,7 +46,9 @@ def select(user: models.User) -> DbOperationResult:
     return sql_engine.select(SQL_SELECT, parms)
 
 
-
+#------------------------------------------------------
+# Update the database's user record to the field values provided in the given User domain model
+#------------------------------------------------------
 def update(user: models.User) -> DbOperationResult:
     parms = (
         user.email,
@@ -48,7 +62,46 @@ def update(user: models.User) -> DbOperationResult:
 
     return sql_engine.modify(SQL_UPDATE, parms)
 
+#------------------------------------------------------
+# Insert the user object into the database
+#
+# Returns: DbOperationResult
+#   the DbOperationResult.data value contains the auto-incremented id for the new user
+#------------------------------------------------------
+def insert(new_user: models.User) -> DbOperationResult:
+    result = DbOperationResult(successful=True)
+    db = ConnectionPrepared()
+
+    parms = _getInsertParms(new_user)
+    
+    try:
+        db.connect()
+        cursor = db.getCursor()
+        
+        cursor.execute(SQL_INSERT, parms)
+        db.commit()
+        
+        result.data = cursor.lastrowid
+
+    except Exception as e:
+        result.error = e
+        result.data = None
+        result.successful = False
+    
+    finally:
+        db.close()
+    
+    return result
 
 
 
+def _getInsertParms(user: models.User) -> tuple:
+    parms = (
+        user.email,
+        user.password,
+        user.name_first,
+        user.name_last,
+        user.birth_date,
+    )
 
+    return parms
--- src/api_wmiys/services/users.py
+++ src/api_wmiys/services/users.py
@@ -1,15 +1,33 @@
+#************************************************************************************
+#
+# This class handles all the business logic for the users resource.
+#
+#************************************************************************************
+
+
 from __future__ import annotations
+from enum import Enum
 
 import flask
+from pymysql.structs import DbOperationResult
 
 from api_wmiys.repository import users as users_repo
 from api_wmiys.domain import models
 from api_wmiys.common import responses
 from api_wmiys.common import serializers
-from pymysql.structs import DbOperationResult
+
 
 
 SQL_ERROR_DUPLICATE_KEY = 1062
+
+#------------------------------------------------------
+# Bad request error messages returned in the response
+#------------------------------------------------------
+class BadResponseErrorMessages(str, Enum):
+
+    MISSING_REQUIRED_FIELD = 'Missing one or more required fields.'
+    EMAIL_IN_USE           = 'The email is already in use'
+
 
 #------------------------------------------------------
 # Retrieve a single user
@@ -44,22 +62,64 @@ def response_PUT(user_id) -> flask.Response:
     if not result.successful:
         return _handleDbError(result)
 
-    output = getUserView(user_id)
+
+    return _standardResponseWithView(user_id, responses.updated)
+
+#------------------------------------------------------
+# Create a new user
+#------------------------------------------------------
+def response_POST() -> flask.Response:
+    new_user = extractUserModelFromForm()
+
+    # verify that all the required fields are set
+    if not areRequiredAttributesSet(new_user):
+        return responses.badRequest(BadResponseErrorMessages.MISSING_REQUIRED_FIELD)
     
-    if not output:
-        return responses.notFound()
+    # attempt to insert the user into the database
+    db_result = users_repo.insert(new_user)
 
-    return responses.updated(output)
+    # if the sql command was not successful, handle it accordingly
+    if not db_result.successful:
+        return _handleDbError(db_result)
+    
+    # a successful database insert command returns the new user's id
+    new_user_id = db_result.data
+
+    # now, call the standardized single user return routine for the new user
+    return _standardResponseWithView(new_user_id, responses.created)
 
 #------------------------------------------------------
-# handle some of the database errors
+# Verifies that all the required fields of the given User object are set (not null)
+# All these fields need to have a value before inserting the object into the database
+#
+# Returns a bool
+#   - true: all the required attributes are set and are not null
+#   - false: one or more of the attributes is null
 #------------------------------------------------------
-def _handleDbError(db_result: DbOperationResult) -> flask.Response:
+def areRequiredAttributesSet(user: models.User) -> bool:
+    required_user_fields = [
+        user.email, 
+        user.password, 
+        user.name_first, 
+        user.name_last, 
+        user.birth_date
+    ]
 
-    if db_result.error.errno == SQL_ERROR_DUPLICATE_KEY:
-        return responses.badRequest('The email is already in use')
+    if None in required_user_fields:
+        return False
+    else:
+        return True
 
-    return responses.badRequest(str(db_result.error))
+
+#------------------------------------------------------
+# Serialize the incoming request form data into a User domain model
+#------------------------------------------------------
+def extractUserModelFromForm() -> models.User:
+    form = flask.request.form.to_dict()
+    serializer = serializers.UserSerializer(form)
+    user_model: models.User = serializer.serialize().model
+
+    return user_model
 
 #------------------------------------------------------
 # Load up an User model with all the current values stored in the database
@@ -80,6 +140,7 @@ def generateUpdatedUserModelFromForm(user_id) -> models.User:
 
     return user_model
 
+
 #------------------------------------------------------
 # Get the user domain model
 #------------------------------------------------------
@@ -94,13 +155,37 @@ def getUserModel(user_id) -> models.User:
 
 
 #------------------------------------------------------
-# Get the user view dict
+# Get the user view dict from the database
 #------------------------------------------------------
 def getUserView(user_id) -> dict | None:
-    user_model = models.User(
-        id = user_id
-    )
-
+    user_model = models.User(id=user_id)
     result = users_repo.select(user_model)
 
     return result.data
+
+#------------------------------------------------------
+# Standard response for returning a single User record
+#
+# Args:
+#   - user_id: the user id
+#   - responses_callback: api_wmiys.common.responses callback to generate the appropriate flask response with headers
+#------------------------------------------------------
+def _standardResponseWithView(user_id, responses_callback) -> flask.Response:
+    output = getUserView(user_id)
+    
+    if not output:
+        return responses.notFound()
+
+    return responses_callback(output)
+
+
+#------------------------------------------------------
+# handle some of the database errors
+#------------------------------------------------------
+def _handleDbError(db_result: DbOperationResult) -> flask.Response:
+
+    if db_result.error.errno == SQL_ERROR_DUPLICATE_KEY:
+        return responses.badRequest(BadResponseErrorMessages.EMAIL_IN_USE)
+
+    return responses.badRequest(str(db_result.error))
+
