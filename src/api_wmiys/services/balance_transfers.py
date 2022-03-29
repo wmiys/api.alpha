6a454612b4ea68c369298970a7bb2f960b182fbd
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-03-29 08:08:03 -0500

    save object to database

--- src/api_wmiys/services/balance_transfers.py
+++ src/api_wmiys/services/balance_transfers.py
@@ -10,38 +10,32 @@ from __future__ import annotations
 from datetime import datetime
 import flask
 
-import stripe
-
 from wmiys_common import utilities
 from api_wmiys.services import users as user_services
 from api_wmiys.common import responses
 from api_wmiys.domain import models
 from api_wmiys import payments
-
+from api_wmiys.repository import balance_transfers as balance_transfers_repo
 
 
 #------------------------------------------------------
 # Create a new balance transfer
 #------------------------------------------------------
 def responses_POST() -> flask.Response:
-    # make sure user has a balance greater than 1 before sending it
     balance_transfer = _createNewModel()
 
-    # check to make sure the user has a stripe account on file and they have sufficient funds
-    if not balance_transfer.destination_account_id:
-        return responses.badRequest('Stripe account is not setup.')
-    elif balance_transfer.amount < 1:
-        return responses.badRequest('Insufficient funds')
-
-    # send their balance to them using stripe
     try:
+        # validate the model before anything else
+        _validateModel(balance_transfer)
+
+        # send the user's funds to them using stripe
         balance_transfer.transfer_id = _sendTransfer(balance_transfer)
+
+        # record the transfer in the database
+        _saveTransferToDatabase(balance_transfer)
+    
     except Exception as e:
         return responses.badRequest(str(e))
-
-    
-    # record the transfer in the database
-
 
     return responses.created(balance_transfer)
 
@@ -70,6 +64,17 @@ def _createNewModel() -> models.BalanceTransfer:
 def _getUserStats() -> dict:
     return user_services.getUserView(flask.g.client_id)
 
+#------------------------------------------------------
+# Validate the given BalanceTransfer:
+#   - client has a stripe account set up
+#   - client has a lender balance greater than 1
+#------------------------------------------------------
+def _validateModel(balance_transfer: models.BalanceTransfer):
+    if not balance_transfer.destination_account_id:
+        raise Exception('Stripe account is not setup.')
+    elif balance_transfer.amount < 1:
+        raise Exception('Insufficient funds')
+
 
 #------------------------------------------------------
 # Send the BalanceTransfer to Stripe and transfer the amount
@@ -81,8 +86,18 @@ def _sendTransfer(balance_transfer: models.BalanceTransfer) -> str:
     if not result.successful:
         raise result.error
 
-    return result.id
+    return result.data.id
 
+#------------------------------------------------------
+# Record the given BalanceTransfer object in the database
+#------------------------------------------------------
+def _saveTransferToDatabase(balance_transfer: models.BalanceTransfer) -> bool:
+    db_result = balance_transfers_repo.insert(balance_transfer)
+
+    if not db_result.successful:
+        raise db_result.error
+
+    return True
 
         
 
