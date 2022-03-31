e1b22ff323f56f10e04392c839df0ae975e07e2f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-03-30 20:45:22 -0500

    remove old model code

--- src/api_wmiys/models/__init__.py
+++ src/api_wmiys/models/__init__.py
@@ -3,6 +3,5 @@ from . import product_categories as product_categories
 from . import product_request as product_request
 
 # classes
-from .product_listing_availability import ProductListingAvailability as ProductListingAvailability
 from .product_request import ProductRequest as ProductRequest
 from .product_request import RequestStatus as RequestStatus
