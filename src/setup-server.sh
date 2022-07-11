74f193f122ff8ff73acb5c7e605916aad152c4a0
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-07-11 18:42:04 +0100

    fix the setup-server bash script

--- src/setup-server.sh
+++ src/setup-server.sh
@@ -7,10 +7,10 @@ mod_wsgi-express setup-server \
 --log-level info   \
 --compress-responses \
 --server-root /etc/api.wmiys.com \
---host 104.225.208.116 \
---document-root /var/www/wmiys/api/api_wmiys/static \
+--host 104.225.208.163 \
+--document-root /var/www/wmiys/api.wmiys/src/api_wmiys/static \
 --setup-only \
-/var/www/wmiys/api/api_wmiys.wsgi
+/var/www/wmiys/api.wmiys/src/api_wmiys.wsgi
 
 # restart the server
 /etc/api.wmiys.com/apachectl restart
